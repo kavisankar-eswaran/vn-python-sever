0db1ef5f2632cee28c33c64a4087df7c8df6ddc6
kavisankar-eswaran <kavisankar.kmp@gmail.com>
2024-01-23 15:28:18 +0530

    String value changes

--- vn-28/main.py
+++ vn-28/main.py
@@ -1,1 +1,9 @@
-print("Server Configuration Success")
+Name = 'Vaan Admin'
+UserID = 1021
+Bot = False
+ResponseTIme = 22.3
+
+print("Username : " +Name)
+print("User ID  : " +str(UserID))
+print("Is the Bot  : " +str(Bot))
+print("Server response time  : " +str(ResponseTIme)+"ms")
