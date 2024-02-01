8a9238324ffd6c8eeb7df307e984289263170fe8
kavisankar-eswaran <kavisankar.kmp@gmail.com>
2024-02-01 06:26:57 +0530

    multiple assignment operator

--- vn-28/main.py
+++ vn-28/main.py
@@ -1,9 +1,16 @@
-Name = 'Vaan Admin'
-UserID = 1021
-Bot = False
-ResponseTIme = 22.3
+# Name = 'Vaan Admin'
+# UserID = 1021
+# Bot = False
+# ResponseTIme = 22.3
 
-print("Username : " +Name)
-print("User ID  : " +str(UserID))
-print("Is the Bot  : " +str(Bot))
-print("Server response time  : " +str(ResponseTIme)+"ms")
+# print("Username : " +Name)
+# print("User ID  : " +str(UserID))
+# print("Is the Bot  : " +str(Bot))
+# print("Server response time  : " +str(ResponseTIme)+"ms")
+
+#valhunter, rolex, jop = "K", 22, False
+valhunter = rolex = jop = 25
+
+print(valhunter)
+print(rolex)
+print(jop)
